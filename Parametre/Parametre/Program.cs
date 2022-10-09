4793c75e65c7d444bcfa60b7054f7f15891ef085
Mehmet SUNAY <85196101+mehmetsunay@users.noreply.github.com>
2022-10-09 23:40:03 +0300

    parametre-temel-ornek

---
+++ Parametre/Parametre/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+
+namespace Parametre
+{
+
+
+
+    internal class Program
+    {
+
+        public static int MaxSayi(int sayi1, int sayi2)
+        {
+            if(sayi1 > sayi2)
+            
+             return sayi1;
+
+            else
+                
+             return sayi2;
+
+
+
+        }
+
+
+
+        static void Main(string[] args)
+        {
+            Console.WriteLine(MaxSayi(300,500));
+
+
+
+
+        }
+    }
+}
